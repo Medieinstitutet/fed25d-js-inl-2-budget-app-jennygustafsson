dcd57927c7f4bf0c2483d9bb9bd4e15b964a7d97
jennygustafsson <Gustafssson.jenny@gmail.com>
2026-02-12 09:28:28 +0100

    test8

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
-const repositoryName = 'fed25d-js--inl-2-budget-app-jennygustafsson';
+const repositoryName = 'fed25d-js-inl-2-budget-app-jennygustafsson';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' 
